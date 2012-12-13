75d4597cd66922beb9f5c00784b3ef06a1f4d60d
Lluvia Rosales <rosales.lluvia@gmail.com>
2012-12-13 13:49:35 -0800

    se arreglo el sitemap

--- _/js/calculadoraDOS.js
+++ _/js/calculadoraDOS.js
@@ -3,15 +3,30 @@ function doCalc(){
 		var costo= $('#costos').val();
 		var peso = $('input[name="peso"]:checked').val();
 		var tamano = $('input[name="tamano"]:checked').val();
-if( peso == 'ligero' && tamano == 'chico' ){
-	
+		var cambio = $('input[name="moneda"]:checked').val();
+
+
+if( peso == 'ligero' && tamano == 'chico' )
+	{
 	total= (costo*2);
+		if ( cambio =='pesos'){
+			total= (total*12.85);
+			}//tipo de cambio
 }
-if ( peso == 'medio' && tamano == 'chico' ){
+if ( peso == 'medio' && tamano == 'chico' )
+	{
 	total= (costo*3);
-}
-if ( peso == 'pesado' && tamano == 'chico' ){
-	total= (costo*4);
-}
+		if ( cambio =='pesos'){
+			total= (total*12.85);
+			}//tipo de cambio
+	}
+if ( peso == 'pesado' && tamano == 'chico' )
+	{
+		total= (costo*4);
+			if ( cambio =='pesos'){
+			total= (total*12.85);
+			}//tipo de cambio
+	}
+
 $("#totals").html(total.toFixed(2));
 }
